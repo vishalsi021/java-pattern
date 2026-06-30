0c32c960574a144f4693b7470a3ec72cc4008cf6
vishalsi021 <vishalsi21212121@gmail.com>
2026-06-30 19:54:44 +0530

    Add Java practice files

---
+++ loop1.java
@@ -0,0 +1,9 @@
+public class loop1 {
+    public static void main(String[] args) {
+        String[] names = { "vishal", "virat", "sachin", "dhoni", "rohit" };
+
+        for (String name : names) {
+            System.out.println("Name: " + name);
+        }
+    }
+}
---
+++ printNumber1.java
@@ -0,0 +1,8 @@
+public class printNumber1{
+    public static void main (String[] args){
+        for (int i=2 ; i<=20; i+=2){
+            System.out.println(i);
+        
+        }
+    }
+}
